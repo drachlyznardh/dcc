a23d5a199e9dd47eaebc9593b671e443263851b0
DrachLyznardh <drachlyznardh@gmail.com>
2010-01-25 15:33:11 +0100

    Pointer to store/heap are now fully operational.

--- interpreter.ml
+++ interpreter.ml
@@ -93,7 +93,6 @@ let rec eval_aexp (e:aexp) (r:env) (s:store) (h:heap): value = match e with
                         | _ ->					match i with Ide(name) -> raise (SYNTAX ("Eval_aexp(Ident): Id not found("^name^")") )
                     )
     | Unref(p)  ->	let (idaddr, depth) = (pntr_get_data p r) in
-    					("\neval_eaxp:unref:pntr_get_data:"^(string_of_value idaddr)^":"^(string_of_int depth));
     					do_deref (depth+1) idaddr s h
     | Ref(i)    ->	get_residence i r
     | Malloc(t) ->	(match t with
